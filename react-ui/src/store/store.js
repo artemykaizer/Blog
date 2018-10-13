34b5618e6b8bdae42d93b6b66c200defdcab7b36
yougotmemadnow <36931816+yougotmemadnow@users.noreply.github.com>
2018-10-13 18:39:02 +0600

    Update store.js

--- react-ui/src/store/store.js
+++ react-ui/src/store/store.js
@@ -1,12 +1,10 @@
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import reducer from '../reducers/index'
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialStore = {}
 
-const store = createStore(reducer,
-                         initialStore, 
-                          compose(applyMiddleware(thunkMiddleware),
-                          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(reducer, initialStore, composeWithDevTools(applyMiddleware(thunkMiddleware)))
 
 export default store 
